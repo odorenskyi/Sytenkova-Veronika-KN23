16ca30b48710c95facf03e1ecfd4df416c569cc4
unknown <kin77444@gmail.com>
2024-05-17 03:37:22 +0300

    Added function for tasks 10.1-10.3

--- lab08/prj/ModulesSytenkova/main.cpp
+++ lab08/prj/ModulesSytenkova/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <math.h>
+#include <fstream>
+#include <string>
+#include <ctime>
+
+using namespace std;
 
 double s_calculation (double x, double z)
 {
@@ -68,4 +73,132 @@ int to_binary (int N)
         }
         return ((((N >> 5) & 1) == 1) ? times1 : times0);
     }
+}
+
+void task_ten_one (string input_file, string output_file)
+{
+    ifstream inFile(input_file);
+    ofstream outFile(output_file);
+
+    if (!inFile || !outFile)
+    {
+        cout << "Помилка відкриття файлу" << endl;
+        return;
+    }
+
+    string word;
+    inFile >> word;
+
+    outFile << ":----------------------------------------------------------:" << endl;
+    outFile << "  Cитенкова Вероніка" << endl;
+    outFile << "  Центральноукраїнський національний технічний університет" << endl;
+    outFile << "  м. Кропивницький, Україна" << endl;
+    outFile << "  2024" << endl;
+    outFile << ":------------------ © All rights reserved -----------------:" << endl;
+    outFile << endl;
+
+    string consonants = "бвгґджзйклмнпрстфхцчшщ";
+    int consonantCount = 0;
+
+    for (char c : word)
+    {
+        char lower_c = tolower(c);
+        for (char consonant : consonants)
+        {
+            if (lower_c == consonant)
+            {
+                consonantCount++;
+                break;
+            }
+        }
+    }
+
+    outFile << "Кількість приголосних літер у вхідному файлі: " << consonantCount << endl;
+
+    string droplet = "Про себе не кажи недобрих слів, "
+                  "Бо має сказане таємну силу. "
+                  "Кажи: «Я сильний, впевнений, щасливий!» "
+                  "І буде саме так, як ти хотів!";
+
+    for (char &c : droplet)
+    {
+        c = tolower(c);
+    }
+    for (char &c : word)
+    {
+        c = tolower(c);
+    }
+
+    bool found = false;
+    for (size_t i = 0; i <= droplet.length() - word.length(); ++i)
+    {
+        size_t j = 0;
+        while (j < word.length() && droplet[i + j] == word[j])
+        {
+            ++j;
+        }
+        if (j == word.length())
+        {
+            found = true;
+            break;
+        }
+    }
+    if (found)
+    {
+        outFile << "Слово \"" << word << "\" знайдено у краплинці Віталія Іващенка." << endl;
+    }
+    else
+    {
+        outFile << "Слово \"" << word << "\" не знайдено у краплинці Віталія Іващенка." << endl;
+    }
+
+    inFile.close();
+    outFile.close();
+}
+
+void task_ten_two(const string& input_file)
+{
+    ifstream inputFile(input_file);
+    if (!inputFile)
+    {
+        cout << "Не вдалося відкрити файл" << endl;
+        return;
+    }
+
+    time_t time_now;
+        time_now = time(NULL);
+
+    inputFile.seekg(0, ios::end);
+    streampos fileSize = inputFile.tellg();
+    inputFile.close();
+
+    ofstream outputFile(input_file, ios::app);
+    if (!outputFile)
+    {
+        cout << "Не вдалося відкрити файл для дозапису" << endl;
+        return;
+    }
+
+    outputFile << "\nКількість символів у вхідному вайлі: " << fileSize << endl;
+    outputFile << "Дата дозапису: " << ctime(&time_now) << endl;
+
+    outputFile.close();
+    cout << "Інформацію успішно дописано у файл: " << input_file << endl;
+}
+
+void task_ten_three(int x, int z, int b, const string& output_file)
+{
+    ofstream outputFile(output_file, ios::app);
+
+    float S = s_calculation(x, z);
+    outputFile << "\nРезультат функції s_calculation: " << S << endl;
+
+    string binary = "";
+    while (b > 0)
+    {
+        int remainder = b % 2;
+        binary = std::to_string(remainder) + binary;
+        b /= 2;
+    }
+    outputFile << "\nb у двійковому коді: " << binary << endl;
 }
