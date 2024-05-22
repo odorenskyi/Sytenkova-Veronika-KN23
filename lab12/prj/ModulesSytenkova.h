7cf86b1e6ed0fb9c33da5d246cc278e91cd1f556
unknown <kin77444@gmail.com>
2024-05-23 01:40:32 +0300

    Added task 12.1

---
+++ lab12/prj/ModulesSytenkova.h
@@ -0,0 +1,48 @@
+#ifndef MODULESSYTENKOVA_H_INCLUDED
+#define MODULESSYTENKOVA_H_INCLUDED
+
+#include <math.h>
+
+using namespace std;
+
+class ClassLab12_S{
+private:
+    float radius;
+    int cabin;
+
+public:
+    ClassLab12_S(float radius = 1.0, int cabin = 1) : radius(radius), cabin(cabin) {}
+
+    void set_radius(float r);
+    void set_cabin(float c);
+
+    float get_radius() const { return radius; }
+    float get_cabin() const { return cabin; }
+    float get_area() const { return M_PI * pow(radius, 2); }
+};
+
+void ClassLab12_S::set_radius(float r)
+{
+    if (r >= 1)
+    {
+        radius = r;
+    }
+    else
+    {
+        cout << "Число занадто мале!" << endl;
+    }
+}
+
+void ClassLab12_S::set_cabin(float c)
+{
+    if (с >= 1)
+    {
+         cabin = c;
+    }
+    else
+    {
+        cout << "Число занадто мале!" << endl;
+    }
+}
+
+#endif // MODULESSYTENKOVA_H_INCLUDED
